b2659f82914c59943ccd6b39792ca97063f4088c
bianhaiming <bianhaiming@gmail.com>
2017-01-02 21:25:50 +0800

    use simple_format helper to decorate

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ has_many :groups
 has_many :post
 has_many :group_relationships
 has_many :participated_groups, :through => :group_relationships, :source => :group
+has_many :posts
     def is_member_of?(group)
       participated_groups.include?(group)
     end
